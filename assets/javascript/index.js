85ed60a815d3da71bb1bfb5bd1abb5586f61db20
A21BraisFD <A21BraisFD@iessanclemente.net>
2023-05-22 21:52:41 +0200

    continuar coa galería

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,6 +1,7 @@
 const $d = document,
     $bntMenu = $d.querySelector(".hamburguer"),
-    $menu = $d.querySelector(".menu")
+    $menu = $d.querySelector(".menu"),
+    $slider = $d.querySelector(".video-container")
 
 const toggleMenu = () => {
     $menu.classList.toggle("translateX")
@@ -11,4 +12,32 @@ const toggleMenu = () => {
     }
 }
 
+// let scrollAmount = 0;
+
+// setInterval(() => {
+
+//     scrollAmount += 500; // Ajusta esta cantidad según sea necesario
+//     $slider.scroll({
+//         left: 500,
+//         behavior: "smooth"
+//     });
+// }, 5000);
+
+// let $slides = $slider.children;
+// console.log($slides);
+// let currentIndex = 0;
+
+// setInterval(() => {
+//     let totalWidth = 0;
+//     for (let i = 0; i <= currentIndex; i++) {
+//         totalWidth += $slides[i].offsetWidth + 20;
+//     }
+//     $slider.scroll({
+//         left: totalWidth,
+//         behavior: "smooth"
+//     });
+//     currentIndex = (currentIndex + 1) % $slides.length;
+// }, 5000);
+
 $bntMenu.addEventListener("click", toggleMenu)
+
